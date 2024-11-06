b00fb54347d733db3c180c3e994e99f5e6c9414e
PsevdonimA <mgame3341@gmail.com>
2024-11-06 21:34:45 +0300

    in main.c code for the program added

--- main.c
+++ main.c
@@ -5,17 +5,60 @@
 
 int main(int argc, char** argv)
 {
-    int arg_len = argc - 1;
-    int* types = (int*)malloc(arg_len * sizeof(int));
+    int arg_len = argc - 1; // number of useful arguments
+    int* types = (int*)malloc(arg_len * sizeof(int)); // array of types for arguments
 
     int len = 0; // buffer
     for (int i = 0; i < arg_len; i++)
     {
         len = strlen(argv[i+1]);
         types[i] = type_check(argv[i+1], len);
-        printf("%d\n", types[i]);
     }
-    printf("%d\n", order_check(types, arg_len));
+    int ans_len = order_check(types, arg_len); // number of answers
+    if (ans_len == -1)
+    {
+        printf("Incorrect arguments or their number!\n");
+        printf("Correct example:\n41 + 33 111 - 8 22 \\* 5 221 %c 111 -k 2\n", '%');
+        printf("Write \\ before *!\n");
+    }
+    else if (ans_len == 0)
+    {
+        printf("Arguments correct, but there is 0 questions.\n");
+    }
+    else
+    {
+        int key = atoi(argv[arg_len]);
+        int* ans_arr = (int*)malloc(ans_len * sizeof(int));
+        int pos = 0; // position of index
+        for (int i = 0; i < ans_len; i++)
+        {
+            pos = (3 * i) + 1;
+            ans_arr[i] = get_ans(atoi(argv[pos]), argv[pos+1][0], atoi(argv[pos+2]));
+            printf("Ответ №%d: %d\n", i+1, ans_arr[i]);
+        }
+        char* result = (char*)malloc(ans_len * sizeof(char)); // result string
+        int ch_num = 0; // int value of char
+        for (int i = 0; i < ans_len; i++)
+        {
+            ch_num = ans_arr[i] - key;
+            if (ch_num >= 0 && ch_num <= 255)
+            {
+                result[i] = (char)ch_num;
+            }
+            else
+            {
+                result[i] = ' ';
+            }
+        }
+        printf("Результирующая строка: ");
+        for (int i = 0; i < ans_len; i++)
+        {
+            printf("%c", result[i]);
+        }
+        printf("\n");
+        free(result);
+        free(ans_arr);   
+    }
     free(types);
     return 0;
 }
